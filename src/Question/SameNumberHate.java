0e21d79c6a9b011dfd06e244b32ea0850a67536e
dpound <dpound@naver.com>
2023-10-20 23:49:15 +0900

    문제 추가 , 같은 숫자는 싫어, 프로그래머스

---
+++ src/Question/SameNumberHate.java
@@ -0,0 +1,18 @@
+package Question;
+
+public class SameNumberHate {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Solution {
+        public int[] solution(int []arr) {
+            int[] answer = {};
+
+            // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+            System.out.println("Hello Java");
+
+            return answer;
+        }
+    }
+}
