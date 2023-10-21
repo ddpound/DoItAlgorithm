cb998990a2f56a95c7a1b9c3ae5bd806b609ec24
dpound <dpound@naver.com>
2023-10-22 01:45:42 +0900

    문제 해결 완료, 같은 숫자가 싫어요

--- src/Question/SameNumberHate.java
+++ src/Question/SameNumberHate.java
@@ -1,33 +1,46 @@
 package Question;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12906
+// 프로그래머스 같은 숫자는 싫어
 
 public class SameNumberHate {
     public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
 
+        for (int i : solution(arr)
+             ) {
+            System.out.print(i + " ");
+        }
     }
-
 
     public static int[] solution(int []arr) {
-        ArrayList<Integer> copyList = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] reusult;
 
-        // 깃 푸시 테스트
-        int number = arr[0];
-        for (int j : arr) {
-            if (number < 0) {
-                number = j;
-            }
-            if (j != number) {
-                copyList.add(number);
-                number = -1;
+
+        for (int i : arr
+             ) {
+            if(stack.isEmpty()){
+                System.out.println("push");
+                stack.push(i);
+            }else if (stack.peek() != i ) {
+                System.out.println("push");
+                stack.push(i);
             }
         }
-        int[] answer = new int[copyList.size()];
-        for (int i = 0; i < copyList.size(); i++) {
-            answer[i] = copyList.get(i);
-        }
 
-        return answer;
+        reusult = new int[stack.size()];
+        for (int i = reusult.length-1; i >= 0; i--) {
+            //System.out.println(stack.pop());
+            reusult[i] = stack.pop();
+        }
+        System.out.println();
+        return reusult;
     }
-
 }
